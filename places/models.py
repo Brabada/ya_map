6d71ddfc4b2a0a35038df503c87aae538a44cbde
Brabada <freemindobject@gmail.com>
2023-06-07 23:35:05 +0300

    Add model for images

--- places/models.py
+++ places/models.py
@@ -29,3 +29,25 @@ class Place(models.Model):
         ordering = ['title']
         verbose_name = 'место'
         verbose_name_plural = 'места'
+
+
+class Image(models.Model):
+    image = models.ImageField(
+        'Изображение',
+        null=True,
+    )
+    place = models.ForeignKey(
+        Place,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='images',
+        verbose_name='Фотографии места',
+    )
+
+    def __str__(self):
+        return self.place.title
+
+    class Meta:
+        ordering = ['place']
+        verbose_name = 'изображение'
+        verbose_name_plural = 'изображения'
