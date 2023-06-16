b94596694009fa95ae6d7973acb44b52ff633301
ramazan_kashipov <ramazan.kashipov@systeme.ru>
2023-06-16 19:39:18 +0300

    Added Image Model to Place model as inline

--- places/admin.py
+++ places/admin.py
@@ -3,8 +3,24 @@ from .models import Place, Image
 # Register your models here.
 
 
-admin.site.register(Place)
-admin.site.register(Image)
+# admin.site.register(Place)
+# admin.site.register(Image)
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline,
+    ]
+
 
 
 
